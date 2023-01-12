b1cbd7d37428d6f58a0f394b5346a2f6d8e1abc2
franco@mumuki.org <franco@mumuki.org>
2023-01-12 16:11:56 +0000

    Mumuki Export on 2023-01-12 16:11:56 +0000

--- 00002_Não me diga!/test.py
+++ 00002_Não me diga!/test.py
@@ -1,33 +1,33 @@
 class Test(unittest.TestCase):
 
-  def test_ejemplo_1(self):
+  def test_exemplo_1(self):
     self.assertFalse(
-      pierde_tabu("esta frase gana", ["blanco", "negro"]),
-      'pierde_tabu("esta frase gana", ["blanco", "negro"]) debería retornar False')
+      perde_tabu("esta frase gana", ["blanco", "negro"]),
+      'perde_tabu("esta frase gana", ["blanco", "negro"]) debería retornar False')
 
-  def test_ejemplo_2(self):
+  def test_exemplo_2(self):
     self.assertTrue(
-      pierde_tabu("esta frase no pierde", ["si", "no"]),
-      'pierde_tabu("esta frase no pierde", ["si", "no"]) debería retornar True')
+      perde_tabu("esta frase no pierde", ["si", "no"]),
+      'perde_tabu("esta frase no pierde", ["si", "no"]) debería retornar True')
 
-  def test_ejemplo_3(self):
+  def test_exemplo_3(self):
     self.assertFalse(
-      pierde_tabu("bajo el nogal", ["arriba", "árbol", "sombra", "nuez"]),
-      'pierde_tabu("bajo el nogal", ["arriba", "árbol", "sombra", "nuez"]) debería retornar False')
+      perde_tabu("bajo el nogal", ["arriba", "árbol", "sombra", "nuez"]),
+      'perde_tabu("bajo el nogal", ["arriba", "árbol", "sombra", "nuez"]) debería retornar False')
 
   def test_variante_1(self):
     self.assertFalse(
-      pierde_tabu("esta frase gana", ["si", "no", "blanco", "negro"]),
-      'pierde_tabu("esta frase gana", ["si", "no", "blanco", "negro"]) debería retornar False')
+      perde_tabu("esta frase gana", ["si", "no", "blanco", "negro"]),
+      'perde_tabu("esta frase gana", ["si", "no", "blanco", "negro"]) debería retornar False')
   
   def test_variante_2(self):
     self.assertTrue(
-      pierde_tabu("esta frase no pierde", ["si", "no", "blanco", "negro"]),
-      'pierde_tabu("esta frase no pierde", ["si", "no", "blanco", "negro"]) debería retornar True')
+      perde_tabu("esta frase no pierde", ["si", "no", "blanco", "negro"]),
+      'perde_tabu("esta frase no pierde", ["si", "no", "blanco", "negro"]) debería retornar True')
   
   def test_variante_3(self):
     self.assertFalse(
-      pierde_tabu("bajo el nogal", ["si", "no", "blanco", "negro"]),
-      'pierde_tabu("bajo el nogal", ["si", "no", "blanco", "negro"]) debería retornar False')
+      perde_tabu("bajo el nogal", ["si", "no", "blanco", "negro"]),
+      'perde_tabu("bajo el nogal", ["si", "no", "blanco", "negro"]) debería retornar False')
 
 

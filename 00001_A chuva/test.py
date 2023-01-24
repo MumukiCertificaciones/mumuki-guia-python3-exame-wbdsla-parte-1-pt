86e002ae9578c82a2260eb5e9c6bd62a35c0382c
franco@mumuki.org <franco@mumuki.org>
2023-01-24 16:08:20 +0000

    Mumuki Export on 2023-01-24 16:08:20 +0000

--- 00001_A chuva/test.py
+++ 00001_A chuva/test.py
@@ -9,7 +9,7 @@ class Test(unittest.TestCase):
   def test_exemplo_3(self):
     self.assertEquals($forecast(80, False), "Amanhã há grandes chances de chuva sem vento")
 
-  def test_mais_de_$corteAlta(_sem_vento(self):
+  def test_mais_de_$corteAlta_sem_vento(self):
     self.assertEquals($forecast($corteAlta+1, False) , "Amanhã há grandes chances de chuva sem vento")
 
   def test_menos_de_$corteAlta_sem_vento(self):

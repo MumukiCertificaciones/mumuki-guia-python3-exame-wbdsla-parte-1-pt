1fbbfa0c4b0b2ec8161586abd672dbde90b54b46
franco@mumuki.org <franco@mumuki.org>
2023-01-24 15:44:58 +0000

    Mumuki Export on 2023-01-24 15:44:58 +0000

--- 00001_A chuva/test.py
+++ 00001_A chuva/test.py
@@ -4,13 +4,13 @@ class Test(unittest.TestCase):
     self.assertEquals($forecast(20, True), "Amanhã há poucas chances de chuva com vento")
 
   def test_exemplo_2(self):
-    self.assertEquals($forecast(40, True), "Amanhã há chances médias de chuva com vento")
+    self.assertEquals($forecast(40, True), "Amanhã há poucas chances de chuva com vento")
 
   def test_exemplo_3(self):
     self.assertEquals($forecast(80, False), "Amanhã há grandes chances de chuva sem vento")
 
   def test_mas_de_$corteAlta(self):
-    self.assertEquals($forecast($corteAlta+1, False), "Amanhã há grandes chances de chuva sem vento")
+    self.assertEquals($forecast($corteAlta+1, False) , "Amanhã há grandes chances de chuva sem vento")
 
   def test_menos_de_$corteAlta(self):
-    self.assertEquals($forecast($corteAlta-1, False), "Amanhã há chances médias de chuva sem vento")
+    self.assertEquals($forecast($corteAlta-1, False), "Amanhã há médias chances de chuva sem vento")

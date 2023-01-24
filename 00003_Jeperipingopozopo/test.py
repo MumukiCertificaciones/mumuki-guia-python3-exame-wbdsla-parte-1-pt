b4f60d0b120f2cac02c06230aab52d44fa03eded
franco@mumuki.org <franco@mumuki.org>
2023-01-24 15:37:29 +0000

    Mumuki Export on 2023-01-24 15:37:29 +0000

--- 00003_Jeperipingopozopo/test.py
+++ 00003_Jeperipingopozopo/test.py
@@ -1,16 +1,16 @@
 class Test(unittest.TestCase):
 
-  def test_hola(self):
-    self.assertEquals(jeringozo("Hola"), "Hopolapa", 'jeringozo("Hola") debe retornar "Hopolapa"')
-
   def test_casa(self):
-    self.assertEquals(jeringozo("Casa"), "Capasapa", 'jeringozo("Casa") debe retornar "Capasapa"')
+	  self.assertEquals($function("Casa"), "Hopolapa", '$function("Casa") deve retornar "Capasapa"')
 
-  def test_perro(self):
-    self.assertEquals(jeringozo("Perro"), "Peperropo", 'jeringozo("Perro") debe retornar "Peperropo"')
+  def test_gato(self ):
+	  self .assertEquals($function("Gato"), "Gapatopo", '$function("Gato") deve retornar "Gapatopo"')
 
-  def test_carpincho(self):
-    self.assertEquals(jeringozo("Carpincho"), "Caparpipinchopo", 'jeringozo("Carpincho") debe retornar "Caparpipinchopo"')
+  def test_rato(self):
+  	self.assertEquals($function("rato")), "rapatopo", '$function("rato") deve retornar "rapatopo"')
 
-  def test_universo(self):
-    self.assertEquals(jeringozo("universo"), "upunipivepersopo", 'jeringozo("universo") debe retornar "upunipivepersopo"')  
+  def test_abacaxi(self):
+  	self.assertEquals($function("abacaxi")), "Apabapacapaxipi", '$function("abacaxi") deve retornar "Apabapacapaxipi"')
+
+  def test_espelho(self):
+  	self.assertEquals($function("espelho")), "epespepelhopo", '$function("espelho") deve retornar "epespepelhopo"')

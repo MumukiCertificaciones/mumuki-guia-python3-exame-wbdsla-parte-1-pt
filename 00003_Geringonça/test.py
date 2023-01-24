3b01770697ac61cee4f769e958e31d84aed5e383
franco@mumuki.org <franco@mumuki.org>
2023-01-24 16:19:35 +0000

    Mumuki Export on 2023-01-24 16:19:35 +0000

--- 00003_Geringonça/test.py
+++ 00003_Geringonça/test.py
@@ -10,7 +10,7 @@ class Test(unittest.TestCase):
   	self.assertEquals($function("rato"), "rapatopo", '$function("rato") deve retornar "rapatopo"')
 
   def test_abacaxi(self):
-  	self.assertEquals($function("abacaxi"), "Apabapacapaxipi", '$function("abacaxi") deve retornar "Apabapacapaxipi"')
+  	self.assertEquals($function("abacaxi"), "apabapacapaxipi", '$function("abacaxi") deve retornar "apabapacapaxipi"')
 
   def test_espelho(self):
   	self.assertEquals($function("espelho"), "epespepelhopo", '$function("espelho") deve retornar "epespepelhopo"')

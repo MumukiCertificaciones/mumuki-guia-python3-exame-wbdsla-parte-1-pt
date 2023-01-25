a91cb8f6016fcec99c4fe8e08a47ffb4005d7cb0
franco@mumuki.org <franco@mumuki.org>
2023-01-25 21:17:11 +0000

    Mumuki Export on 2023-01-25 21:17:11 +0000

--- 00002_Não me diga!/test.py
+++ 00002_Não me diga!/test.py
@@ -2,35 +2,35 @@ class Test(unittest.TestCase):
 
   def test_exemplo_1(self):
   	self.assertFalse(
-    	perde_tabu("essa frase vence", ["branco", "preto"]),
-  	    'perde_tabu("essa frase vence", ["branco", "preto"]) deve retornar False')
+    	e_tabu("essa frase vence", ["branco", "preto"]),
+  	    'e_tabu("essa frase vence", ["branco", "preto"]) deve retornar False')
 
   def test_exemplo_2(self):
   	self.assertTrue(
-    	perde_tabu("essa frase não vence", ["sim", "não"]),
-  	    'perde_tabu("essa frase não vence", ["sim", "não"]) deve retornar True')
+    	e_tabu("essa frase não vence", ["sim", "não"]),
+  	    'e_tabu("essa frase não vence", ["sim", "não"]) deve retornar True')
 
   def test_exemplo_3(self):
   	self.assertFalse(
-    	perde_tabu("embaixo a nogueira", ["acima", "arvore", "sombra", "noz"]),
-  	    'perde_tabu("embaixo a nogueira", ["acima", "arvore", "sombra", "noz"]) deve retornar False')
+    	e_tabu("embaixo a nogueira", ["acima", "arvore", "sombra", "noz"]),
+  	    'e_tabu("embaixo a nogueira", ["acima", "arvore", "sombra", "noz"]) deve retornar False')
 
   def test_variante_1(self):
   	self.assertFalse(
-    	perde_tabu("essa frase vence", ["sim", "não", "branco", "preto"]),
-  	    'perde_tabu("essa frase vence", ["sim", "não", "branco", "preto"]) deve retornar False')
+    	e_tabu("essa frase vence", ["sim", "não", "branco", "preto"]),
+  	    'e_tabu("essa frase vence", ["sim", "não", "branco", "preto"]) deve retornar False')
  
   def test_variante_2(self):
   	self.assertTrue(
-    	perde_tabu("essa frase não vence", ["sim", "não", "branco", "preto"]),
-  	    'perde_tabu("essa frase não vence", ["sim", "não", "branco", "preto"]) deve retornar True')
+    	e_tabu("essa frase não vence", ["sim", "não", "branco", "preto"]),
+  	    'e_tabu("essa frase não vence", ["sim", "não", "branco", "preto"]) deve retornar True')
  
   def test_variante_3(self):
   	self.assertFalse(
-    	perde_tabu("embaixo a nogueira", ["sim", "não", "branco", "preto", "em"]),
-  	    'perde_tabu("embaixo a nogueira", ["sim", "não", "branco", "preto", "em"]) deve retornar False')
+    	e_tabu("embaixo a nogueira", ["sim", "não", "branco", "preto", "em"]),
+  	    'e_tabu("embaixo a nogueira", ["sim", "não", "branco", "preto", "em"]) deve retornar False')
 
   def test_variante_(self):
   	self.assertTrue(
-    	perde_tabu("quem perde", ["vence", "perde"]),
-  	    'perde_tabu("quem perde", ["vence", "perde"]) deve retornar True')        
+    	e_tabu("quem perde", ["vence", "perde"]),
+  	    'e_tabu("quem perde", ["vence", "perde"]) deve retornar True')        
